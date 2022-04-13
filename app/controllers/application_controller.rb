d4b0b1c67ee704285befd4dd4e843b83f214bbcd
umer-iqbal-brainx <umer.iqbal@brainxtech.com>
2022-04-13 22:51:48 +0500

    LogIn and signUp form down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation, :description, :avatar])
   end
-
-  # before_filter :configure_sanitized_params, if: :devise_controller?
-  #
-  # def configure_sanitized_params
-  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :username, :password, :password_confirmation, :avatar) }
-  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :username, :password, :password_confirmation, :avatar) }
-  # end
-
+  
 end
