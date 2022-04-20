c7eb1312247f1cfd7c726998d5567690f9fc78fc
umer-iqbal-brainx <umer.iqbal@brainxtech.com>
2022-04-20 10:17:28 +0500

    writing new article is complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,58 @@
 class UsersController < ApplicationController
   include Pagy::Backend
 
-def show
+  def index
+    # @articles = Article.all
+
+    # Article.ransack(params[:q])
+    # @pagy, @articles = pagy(@q.result, items: 4)
+
+    # @pagy, @articles = pagy(Article.all, items: 4)
+    # debugger
+  end
+
+  def show
     @userArticles = User.find(params[:id]).articles
     @q = @userArticles.ransack(params[:q])
     @pagy, @articles = pagy(@q.result, items: 4)
     # debugger
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    debugger
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, images: [])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
-  has_one_attached :images
+  has_rich_text :body
+  has_one_attached :thumbnail
   belongs_to :user
   has_many :comments
   has_many :admin_topics_lists
 
   validates :title, presence: true
   validates :body, presence: true
+  validates :thumbnail, presence: true
 
 end
